34523a8c818e49781bf58f4b60f6a8ef6f3c98de
mikewurtz <mikewurtz1@gmail.com>
2025-04-14 11:34:34 -0500

    fix: improve WaitForTasks()

--- internal/task/manager/manager.go
+++ internal/task/manager/manager.go
@@ -32,27 +32,40 @@ func (tm *TaskManager) addTask(task *Task) {
 
 // WaitForTasks waits for all tasks to complete with a timeout
 func (tm *TaskManager) WaitForTasks() error {
-	tm.mu.RLock()
-	tasks := make([]*Task, 0, len(tm.tasksMapByID))
-	for _, task := range tm.tasksMapByID {
-		tasks = append(tasks, task)
-	}
-	tm.mu.RUnlock()
+    tm.mu.RLock()
+    tasks := make([]*Task, 0, len(tm.tasksMapByID))
+    for _, task := range tm.tasksMapByID {
+        tasks = append(tasks, task)
+    }
+    tm.mu.RUnlock()
 
-	// 30 seconds should be plenty of time for tasks to terminate and clean up
-	timeout := time.After(30 * time.Second)
+    var wg sync.WaitGroup
+    wg.Add(len(tasks))
 
-	for _, task := range tasks {
-		select {
-		case <-task.done:
-			continue
-		case <-timeout:
-			return fmt.Errorf("timeout waiting for tasks to complete")
-		}
-	}
+	// send done to all the tasks
+    for _, task := range tasks {
+        go func(t *Task) {
+            <-t.done
+            wg.Done()
+        }(task)
+    }
 
-	return nil
+    // Create a done channel that signals when all tasks are complete
+    done := make(chan struct{})
+    go func() {
+        wg.Wait()
+        close(done)
+    }()
+
+	// wait until all the taks are done or we timeout
+    select {
+    case <-done:
+        return nil
+    case <-time.After(30 * time.Second):
+        return fmt.Errorf("timeout waiting for tasks to complete")
+    }
 }
+
 
 func (tm *TaskManager) getTaskFromMap(taskID string) (*Task, error) {
 	tm.mu.RLock()
